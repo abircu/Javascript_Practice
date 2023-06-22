ee4c115a91c4d7a7045c8251c820ba00ef615ccd
abircu <mdabircse4873@gmail.com>
2023-06-22 15:11:48 +0600

    Loop Practice in javasript

---
+++ For_Loop.js
@@ -0,0 +1,6 @@
+const cars=["toyta","bolbo","pradu","suzuki"];
+const cycle=["phonix","duranto"];
+const total=cars.concat(cycle);
+for(let i=0; i<total.length; i++){
+    console.log(total);
+}
---
+++ For_of.js
@@ -0,0 +1,6 @@
+const obj=[ "Sohel","rana","mdabircse4873@gmail.com"]; 
+const num=[10,20,21,30,32,56,589];
+const total=onj.concate(num);
+for (let i of total) {
+    console.log(i);
+}
